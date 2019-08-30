da197f70f1c0f83ca6dd63ad1e5497cc3395933f
Sebastian Rupprecht <sebastian.rupprecht@saxms-team.de>
2019-08-30 15:05:41 +0200

    foo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { HealtComponent } from './ecs/components/health.component';
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { Perceptron } from './neural-net/neural-net';
-import { Scene } from './scene/scene';
+import { Game } from '@app/game/game';
+import { PositionComponent } from '@ecs/components/position.component';
+import { RenderComponent, RenderType } from '@ecs/components/render.component';
+import { Entity } from '@ecs/core/entity';
+import { RenderSystem } from '@ecs/systems/render.system';
+import { Perceptron } from '@nn/neural-net';
+import { Point2D } from './game/point';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +13,53 @@ import { Scene } from './scene/scene';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  private width: number;
+  private height: number;
+  private brain: Perceptron;
+  private game: Game;
 
-  constructor(private ref: ElementRef) { }
+  constructor(private ref: ElementRef) {
+    this.width = this.height = 400;
+  }
 
   ngOnInit(): void {
-    const scene: Scene = new Scene(this.ref);
+    this.game = new Game(this.ref);
+    this.game.setSize(this.width, this.height);
 
-    const perc: Perceptron = new Perceptron();
-    const inputs: number[] = [-1, 0.5];
-    const guess: number = perc.guess(inputs);
-    console.log('guess', guess);
-    const health = new HealtComponent();
-    console.log(health)
+    this.brain = new Perceptron();
+
+    for (let i = 0; i < 1000; i++) {
+      const x: number = Math.floor(Math.random() * this.width) + 0;
+      const y: number = Math.floor(Math.random() * this.height) + 0;
+      this.game.ecs.createEntity(new PositionComponent(new Point2D(x, y)), new RenderComponent('grey', 'grey', RenderType.POINT));
+    }
+
+    this.game.ecs.addSystem(new RenderSystem());
+    this.train(true)
+
+  }
+
+  train(skip?: boolean) {
+    const start: number = Math.floor(Math.random() * this.game.ecs.getEntities().length - 10) + 0;
+    this.game.ecs.getEntities().filter((entity: Entity) => entity.hasComponents('PositionComponent', 'RenderComponent'))
+      .forEach((entity: Entity, index: number) => {
+        const pos: PositionComponent = entity.getComponent('PositionComponent') as PositionComponent;
+        const renderer: RenderComponent = entity.getComponent('RenderComponent') as RenderComponent;
+        const target: number = pos.getPosition().getX() > pos.getPosition().getY() ? 1 : -1;
+
+        if (!skip) {
+          if (index >= start && index <= start + 3) {
+            this.brain.train([pos.getPosition().getX(), pos.getPosition().getY()], target);
+          }
+        }
+
+        const guess: number = this.brain.guess([pos.getPosition().getX(), pos.getPosition().getY()]);
+        if (guess === target) {
+          renderer.borderColor = 'green';
+        } else {
+          renderer.borderColor = 'red';
+        }
+      });
+
   }
 }
--- src/app/neural-net/neural-net.ts
+++ src/app/neural-net/neural-net.ts
@@ -1,22 +1,36 @@
 
 export class Perceptron {
   private weights: number[];
+  private lr = 0.1; // learning rate
 
   constructor() {
     this.weights = new Array<number>(2);
 
     // init weights randomly
     for (let i = 0; i < this.weights.length; i++) {
-      this.weights[i] = Math.floor(Math.random() * 1) - 1;
+      const rand: number = Math.floor(Math.random() * 200);
+      this.weights[i] = rand > 100 ? (rand * -1) / 100 : rand / 100;
     }
   }
 
   public guess(inputs: number[]): number {
-    const sum = this.weights
-      .reduce((accu: number, currentWeight: number, index: number) =>
-        accu + (inputs[index] * currentWeight));
+    let sum = 0;
+    for (let i = 0; i < this.weights.length; i++) {
+      sum += inputs[i] * this.weights[i];
+    }
 
     return this.sign(sum);
+  }
+
+  public train(inputs: number[], target: number) {
+    const guess: number = this.guess(inputs);
+    const error: number = target - guess;
+
+    // tune weights
+    for (let i = 0; i < this.weights.length; i++) {
+      // gradient descent
+      this.weights[i] += error * inputs[i] * this.lr;
+    }
   }
 
 
